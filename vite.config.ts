ca55792525fa6a988031b6737d2d44f11fa225eb
solome <solome@outlook.com>
2022-01-14 11:52:29 +0800

    fix: 完善`vite`配置,兼容部分低端浏览器

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [react()],
+  plugins: [react(), legacy({
+    targets: ['ie >= 11'],
+    additionalLegacyPolyfills: ['regenerator-runtime/runtime']
+  })],
   build: {
-    outDir: 'docs'
+    outDir: 'docs',
+    target: 'chrome54'
   }
 })
